ebca11a5354efd5548090b74cc71f438e9e4a9c8
jisooyoon <yoonmallang@gmail.com>
2025-08-16 16:27:18 +0900

    🎉 프로젝트 초기 생성

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["react", "@typescript-eslint", "jsx-a11y", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier", // prettier와 충돌 방지
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    // 여기에 필요 시 추가 규칙 설정
+    "react/react-in-jsx-scope": "off", // React 17+ JSX 자동 설정
+  },
+};
