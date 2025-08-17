4e2150d966d806ff3b6092a5b3558ff3947d6372
BK <3031011@naver.com>
2025-08-17 12:40:59 +0900

    ✨ queryClient 활용한 api 공통 에러 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import useApiError from '@/hooks/useApiError';
+import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return <></>;
+  const { handleError } = useApiError();
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      mutations: {
+        onError: handleError,
+      },
+    },
+    queryCache: new QueryCache({
+      onError: handleError,
+    }),
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer position="bottom-center" limit={1} autoClose={4000} hideProgressBar />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
