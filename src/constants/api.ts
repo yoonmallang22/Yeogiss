a03918974e8d4fb8c8c8c01889c570596728f5c4
BK <3031011@naver.com>
2025-08-17 15:40:14 +0900

    🎨 axios instance 생성 / baseURL axios 내부 관리하도록 개선

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,10 +1,8 @@
-const API_BASE_URL = import.meta.env.VITE_SERVER_URL;
-
 const API_VERSION = 'v1';
 
 const ENDPOINTS = {
-  GET_BINS: API_BASE_URL + 'api/' + API_VERSION + '/bins/nearby',
-  GET_ROUTES: API_BASE_URL + 'api/' + API_VERSION + '/routes',
+  GET_BINS: 'api/' + API_VERSION + '/bins/nearby',
+  GET_ROUTES: 'api/' + API_VERSION + '/routes',
 };
 
 export default ENDPOINTS;
