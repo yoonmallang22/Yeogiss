760637796c08c996178633ce472f4139a21ae831
jisooyoon <yoonmallang@gmail.com>
2025-08-30 19:00:28 +0900

    ♻️ 카카오맵 컴포넌트 분리

---
+++ src/components/KakaoMap.tsx
@@ -0,0 +1,25 @@
+import { Map } from "react-kakao-maps-sdk";
+import useKakaoLoader from "@/hooks/useKakaoLoader";
+import useUserLocation from "@/hooks/useUserLocation";
+import { DEFAULT_POSITION } from "@/constants/geo";
+import { Outlet } from "react-router-dom";
+import UserLocationControl from "@/components/userLocationControl/UserLocationControl";
+
+const KakaoMap = () => {
+  useKakaoLoader();
+  const userLocation = useUserLocation();
+
+  return (
+    <Map
+      center={userLocation ?? DEFAULT_POSITION}
+      className="w-full h-screen"
+      level={3}
+      isPanto={true}
+    >
+      <UserLocationControl />
+      <Outlet />
+    </Map>
+  );
+};
+
+export default KakaoMap;
