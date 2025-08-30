b72359787b58e75831dea183be2deb0106ddc71f
KwanBeom <126222927+KwanBeom@users.noreply.github.com>
2025-08-30 15:11:46 +0900

    ✨ 쓰레기통 위치 마커 표시 및 클릭시 정보 표출 구현

---
+++ src/pages/home/components/LoadBinsButton.tsx
@@ -0,0 +1,83 @@
+import reloadImage from "@/assets/restart.svg";
+import { getNearbyBins, type Bin } from "@/lib/api/bin";
+import { useContext, useEffect, useRef, useState } from "react";
+import { KakaoMapContext } from "react-kakao-maps-sdk";
+import { toast } from "react-toastify";
+
+/**
+ * 쓰레기통 추가 로드 기능
+ */
+const LoadBinsButton = ({
+  onLoaded,
+  showThreshold = 500,
+}: {
+  onLoaded?: (bins: Bin[]) => void;
+  showThreshold?: number;
+}) => {
+  const [show, setShow] = useState(false);
+  const kakaoMap = useContext(KakaoMapContext);
+  // 이전 지도 중심에 대한 좌표를 기억한다.
+  const lastCenterRef = useRef<kakao.maps.LatLng | null>(null);
+
+  useEffect(() => {
+    if (!kakaoMap) return;
+    lastCenterRef.current = kakaoMap.getCenter();
+
+    // 드래그 이벤트: 캐싱된 중심 값에서 몇 m 이동했는지 체크하고, 임계치를 초과하면 화면에 표시
+    window.kakao.maps.event.addListener(kakaoMap, "idle", () => {
+      const newCenter = kakaoMap.getCenter();
+      const prevCenter = lastCenterRef.current;
+
+      if (!prevCenter) {
+        lastCenterRef.current = newCenter;
+        return;
+      }
+
+      // 거리 계산 (m 단위)
+      const line = new kakao.maps.Polyline({
+        path: [prevCenter, newCenter],
+      });
+      const distance = line.getLength();
+
+      if (distance >= showThreshold) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+  }, [kakaoMap, showThreshold]);
+
+  if (!show) return null;
+
+  return (
+    <button
+      className="absolute top-4 left-1/2 -translate-x-1/2 bg-white px-4 py-2 rounded-full shadow-md z-10 flex items-center gap-2 text-sm cursor-pointer"
+      onClick={() => {
+        lastCenterRef.current = kakaoMap.getCenter();
+        setShow(false);
+        const bounds = kakaoMap.getBounds();
+        const sw = bounds.getSouthWest(); // 남서쪽
+        const ne = bounds.getNorthEast(); // 북동쪽
+        const line = new kakao.maps.Polyline({ path: [sw, ne] });
+        const diagonal = line.getLength(); // 미터 단위
+
+        // get api 통신
+        getNearbyBins(
+          kakaoMap.getCenter().getLat(),
+          kakaoMap.getCenter().getLng(),
+          Math.ceil(diagonal / 2),
+        ).then((response) => {
+          if (response.data.length === 0) {
+            toast.info("해당 지역에 등록된 쓰레기통이 없어요");
+          } else {
+            if (onLoaded) onLoaded(response.data);
+          }
+        });
+      }}
+    >
+      <img src={reloadImage} alt="쓰레기통 더보기" />이 위치에서 다시 찾기
+    </button>
+  );
+};
+
+export default LoadBinsButton;
