4a76cfaa1155d655351872e4faba5ab6613f99d4
BK <3031011@naver.com>
2025-08-30 20:15:49 +0900

    🐛 길찾기 화면 UserLocatioControl 컴포넌트 관련 상태 버그 수정

--- src/components/userLocationControl/UserLocationControl.tsx
+++ src/components/userLocationControl/UserLocationControl.tsx
@@ -6,19 +6,23 @@ import useGeoPermission from "@/hooks/useGeoPermission";
 import MyMarker from "@/components/userLocationControl/MyMarker";
 import MeButton from "@/components/userLocationControl/MeButton";
 import { UserLocationControlContext } from "@/components/userLocationControl/UserLocationControl.context";
+import { useLocation } from "react-router-dom";
 
 const UserLocationControl = ({ children }: { children: ReactNode }) => {
   const [isFollowing, setIsFollowing] = useState(false);
   const [isLocationButtonFloat, setIsLocationButtonFloat] = useState(false);
 
+  const location = useLocation();
   const kakaoMap = useContext(KakaoMapContext);
   const userLocation = useUserLocation();
   const permission = useGeoPermission();
 
   /* 권한 상태 변화 시 자동 추적 모드 제어 */
   useEffect(() => {
-    setIsFollowing(permission === "granted");
-  }, [permission]);
+    if (location.pathname === "/") {
+      setIsFollowing(permission === "granted");
+    }
+  }, [permission, location]);
 
   /* 자동 추적 모드일 때만 사용자 위치로 중심 좌표 갱신 */
   useEffect(() => {
@@ -47,6 +51,14 @@ const UserLocationControl = ({ children }: { children: ReactNode }) => {
     });
   }, [kakaoMap]);
 
+  useEffect(() => {
+    // 길찾기 화면인 경우
+    if (location.pathname === "/directions") {
+      setIsLocationButtonFloat(true); // 항상 버튼이 float
+      setIsFollowing(false); // 내 위치 추적 off
+    }
+  }, [location, setIsLocationButtonFloat]);
+
   return (
     <UserLocationControlContext.Provider
       value={{
@@ -57,10 +69,7 @@ const UserLocationControl = ({ children }: { children: ReactNode }) => {
       }}
     >
       {userLocation && <MyMarker myLocation={userLocation} />}
-      <MeButton
-        onClick={handleMeButtonClick}
-        isFollowing={isFollowing}
-      />
+      <MeButton onClick={handleMeButtonClick} isFollowing={isFollowing} />
       {children}
     </UserLocationControlContext.Provider>
   );
