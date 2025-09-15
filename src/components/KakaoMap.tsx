7a537be432ae61b36b31f7cb56748a6449ff8bf0
jisooyoon <yoonmallang@gmail.com>
2025-09-15 16:45:30 +0900

    🐛 위치 호출 중복 오류 수정

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -3,15 +3,24 @@ import useKakaoLoader from "@/hooks/useKakaoLoader";
 import { DEFAULT_POSITION } from "@/constants/geo";
 import { Outlet } from "react-router-dom";
 import UserLocationControl from "@/components/userLocationControl/UserLocationControl";
-import useUserLocationOnce from "@/hooks/useUserLocationOnce";
 import useUserLocation from "@/hooks/useUserLocation";
+import { useEffect, useState } from "react";
 
 const KakaoMap = () => {
   useKakaoLoader();
-  const center = useUserLocationOnce();
-  const userLocation = useUserLocation();
 
-  if (center === null || userLocation === null) {
+  const userLocation = useUserLocation();
+  const [center, setCenter] = useState(DEFAULT_POSITION);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (userLocation && loading) {
+      setCenter(userLocation);
+      setLoading(false);
+    }
+  }, [userLocation, loading]);
+
+  if (userLocation === null || loading) {
     return <Map center={DEFAULT_POSITION} className="w-full h-screen" />;
   }
 
