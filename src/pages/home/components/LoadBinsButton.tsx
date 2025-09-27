8f0a049a62ea5e5a3977c95f84b27b1cbcf56afc
BK <3031011@naver.com>
2025-09-27 14:15:15 +0900

    🐛 쓰레기통 조회 결과 없는 경우 토스트 메시지 수정

--- src/pages/home/components/LoadBinsButton.tsx
+++ src/pages/home/components/LoadBinsButton.tsx
@@ -93,8 +93,8 @@ const LoadBinsButton = ({
           Math.ceil(diagonal / 2),
         ).then((response) => {
           if (response.data.length === 0) {
-            toast.info(
-              "현재 위치에는 쓰레기통이 없어요. 조금만 더 걸어가 볼까요?",
+            toast(
+              "⚠ 현재 위치에는 쓰레기통이 없어요. 조금만 더 걸어가 볼까요?",
             );
           } else {
             if (onLoaded) onLoaded(response.data);
