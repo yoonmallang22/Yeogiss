94e23f3afbe62c55d1527e095533027395b5e448
JISOO <yoonmallang@gmail.com>
2026-06-11 22:16:17 +0900

    [✨ feat] 디자인 v1.3 반영 (#17)
    * 🔧 이미지 변경
    
    * 💄 내 위치 마커 디자인 변경 및 애니메이션 추가
    
    * 💄 길찾기 경로 색상 변경
    
    * 💄 디자인 수정

--- src/pages/home/components/BinInfoCard.tsx
+++ src/pages/home/components/BinInfoCard.tsx
@@ -1,7 +1,7 @@
 import type { Bin } from "@/lib/api/bin";
-import walkImage from "@/assets/bx_walk.png";
-import recycleBinIcon from "@/assets/recycle-bin-icon.png";
-import regularBinIcon from "@/assets/regular-bin-icon.png";
+import walkImage from "@/assets/bx_walk.svg";
+import recycleBinIcon from "@/assets/recycle-icon.svg";
+import regularBinIcon from "@/assets/regular-icon.svg";
 import type { LatLng } from "@/types/geolocation.type";
 import { secondsToHMS } from "@/utils/time";
 import { metersToKilometers } from "@/utils/geo";
@@ -36,23 +36,23 @@ const BinInfoCard = ({
   return (
     <BottomCardWithMeBtnFloat onClose={onClose}>
       {/* 위치명 */}
-      <p className="font-medium text-base truncate">
+      <p className="font-medium text-base truncate mb-3">
         {bin.detailAddress ? bin.detailAddress : "쓰레기통(정보 없음)"}
       </p>
 
       {/* 설치 위치 */}
-      <div className="flex items-center gap-2 text-sm">
+      <div className="flex items-center gap-2 text-sm ml-2 mb-3">
         {bin.type === "GENERAL" ? (
-          <img className="w-4 h-5" src={regularBinIcon} />
+          <img className="w-[24px] h-[24px]" src={regularBinIcon} />
         ) : (
-          <img className="w-5 h-5" src={recycleBinIcon} />
+          <img className="w-[24px] h-[24px]" src={recycleBinIcon} />
         )}
         <span>{bin.roadAddress}</span>
       </div>
 
       <div className="flex items-center justify-between mt-2">
         <div className="flex items-center gap-2">
-          <img className="w-8 h-8" src={walkImage} />
+          <img src={walkImage} />
           <div>
             {arrivedSeconds &&
               (() => {
